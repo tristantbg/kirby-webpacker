173ab981b7741056a938dda9801accb1893c3ef3
Tristan Bagot <tristan.bagot@gmail.com>
2018-09-06 18:51:08 +0200

    webpack for all

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -4,23 +4,37 @@
 
 </div>
 
-<?php if($site->googleanalytics()->isNotEmpty()): ?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115150448-2"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+</div>
 
-		gtag('config', '<?= $site->googleanalytics() ?>');
-	</script>
+<div id="outdated">
+  <div class="inner">
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser.
+  <br>Please <a href="http://outdatedbrowser.com" target="_blank">upgrade your browser</a> to improve your experience.</p>
+  </div>
+</div>
+<div id="outdated"><!-- handled by assets/js/vendor/outdated lib !--></div>
+
+<?php if($site->googleanalytics()->isNotEmpty()): ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $site->googleanalytics() ?>"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '<?= $site->googleanalytics() ?>');
+  </script>
 <?php endif ?>
 
 <script>
-	var $sitetitle = '<?= $site->title()->escape() ?>';
+  var $sitetitle = '<?= $site->title()->escape() ?>';
 </script>
 
-<?= js('assets/js/build/app.min.js'); ?>
+<?php if(c::get('environnement') == 'dev'): ?>
+  <script src="http://localhost:8080/assets/bundle.js"></script>
+<?php else: ?>
+  <?= js('assets/build/bundle.js'); ?>
+<?php endif ?>
 
 </body>
 </html>
